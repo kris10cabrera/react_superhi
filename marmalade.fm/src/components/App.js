7ac3a06d6fa6d8488deb323413ce16702c19e149
Kristen Cabrera <cabrera.kristen@gmail.com>
2019-11-14 22:39:57 -0500

    marmalade fm progress

--- marmalade.fm/src/components/App.js
+++ marmalade.fm/src/components/App.js
@@ -1,21 +1,33 @@
 import React, {Component} from 'react';
 import FeaturedMix from './FeaturedMix';
 import Header from './Header';
+import {
+  BrowserRouter as Router, 
+  Route
+} from 'react-router-dom'
 
+const Home = () => <h1>Home</h1>;
+const Archive = () => <h1>Archive</h1>;
+const About = () => <h1>About</h1>;
 
 
 function App() {
   return (
-  <div>
-    <div>
-    <FeaturedMix />
+    <Router>
       <div>
-        <Header/>
-        {/* Routed page */}
+        <div className="flex-l justify-end">
+          <FeaturedMix />
+            <div className="w-50-l relative z-1">
+              <Header/>
+              <Route exact path="/" component={Home}/>
+              <Route path="/archive" component={Archive}/>
+              <Route path="/about" component={About}/>
+              {/* Routed page */}
+            </div>
+        </div>
+        <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Filly_live%2Fola-favela%2F" frameBorder="0" className="player db fixed bottom-0"></iframe>
       </div>
-    </div>
-    <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Filly_live%2Fola-favela%2F" frameBorder="0" ></iframe>
-  </div>
+  </Router>
   );
 }
 
