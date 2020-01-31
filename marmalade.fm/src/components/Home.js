d229771b8160d8ce9ade4c8b9485e24818c78147
kris10cabrera <cabrera.kristen@gmail.com>
2020-01-31 14:06:42 -0500

    update home element and mixjs

--- marmalade.fm/src/components/Home.js
+++ marmalade.fm/src/components/Home.js
@@ -4,7 +4,10 @@ import Mix from './Mix';
 const Home = props =>  (
   <div className="flex flex-wrap justify-between mixes ph3 ph4-1">
     <div className="mix mb4">
-      <Mix name='Bonobo mix' {...props} />
+      <Mix name='Aphex Twin' id='/TheVinylFactory/vf-mix-63-aphex-twin-by-μ-ziq/' {...props} />
+    </div>
+    <div className="mix mb4">
+      <Mix name='Debonair' id='/TheVinylFactory/the-vinyl-factory-radio-debonair/' {...props} />
     </div>
   </div>
 );
--- marmalade.fm/src/components/Mix.js
+++ marmalade.fm/src/components/Mix.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import PlayButton from './PlayButton';
+import PlayMix from './PlayMix';
 
-const Mix = ({name, playMix, currentMix}) => (
-  <div className="aspect-ratio aspect-ratio--3x4 pointer bg-black"
-  onClick={()=> playMix('/TheVinylFactory/vf-mix-63-aphex-twin-by-μ-ziq/')}>
-    <div className="ph3 pv4 aspect-ratio--object mix-overlay">
-      <div className="flex items-center relative z-2">
-        <h1 className="f4 f3-l mv0 white ttu biryani pr2 lh-title">{name}</h1>
-        <PlayButton />
+// passing props to Mix, then passing props to PlayMix
+const Mix = ({ name, ...props}) => (
+  <div className="aspect-ratio aspect-ratio--3x4 pointer bg-black">
+    <PlayMix {...props}>
+      <div className="ph3 pv4 aspect-ratio--object mix-overlay">
+        <div className="flex items-center relative z-2">
+          <h1 className="f4 f3-l mv0 white ttu biryani pr2 lh-title">{name}</h1>
+          <PlayButton />
+        </div>
       </div>
-    </div>
+    </PlayMix>
   </div>
 );
 
